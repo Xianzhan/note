e093e10fd5d464bcffa5efe56d57610fa4aaf490
xianzhan <424447832@qq.com>
2020-07-14 21:22:49 +0800

    add: Spring

---
+++ spring/spring-core/src/main/java/xianzhan/spring/bean/ABean.java
@@ -0,0 +1,15 @@
+package xianzhan.spring.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xianzhan
+ * @since 2020-07-13
+ */
+@Component
+public class ABean {
+
+    public ABean() {
+        System.out.println("ABean");
+    }
+}
---
+++ spring/spring-core/src/main/java/xianzhan/spring/config/ScanPackage.java
@@ -0,0 +1,11 @@
+package xianzhan.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author xianzhan
+ * @since 2020-07-13
+ */
+@ComponentScan("xianzhan.spring")
+public class ScanPackage {
+}
