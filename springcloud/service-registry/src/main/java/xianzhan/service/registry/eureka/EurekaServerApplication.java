f970629da5ddf54b38439c6e6f46325d460006e7
xianzhan <424447832@qq.com>
2020-09-05 14:16:55 +0800

    feat: 服务注册高可用

--- springcloud/service-registry/src/main/java/xianzhan/service/registry/eureka/EurekaServerApplication.java
+++ springcloud/service-registry/src/main/java/xianzhan/service/registry/eureka/EurekaServerApplication.java
@@ -12,6 +12,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
 
+    /**
+     * 选择下面参数其一启动, 高可用注册中心
+     *
+     * @param args --spring.profiles.active=eureka-server-01
+     *             --spring.profiles.active=eureka-server-02
+     * @implNote application.yml
+     */
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
