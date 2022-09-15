4e89c7c8275c76e11e85f8993a578aaf1b192720
xianzhan <424447832@qq.com>
2022-09-15 22:41:43 +0800

    test: jmh 测试模式

---
+++ frame-benchmarks/jmh/src/main/java/xianzhan/frame/jmh/JMH03BenchmarkModes.java
@@ -0,0 +1,100 @@
+package xianzhan.frame.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
+public class JMH03BenchmarkModes {
+
+    /**
+     * 测试每秒执行次数
+     *
+     * @throws InterruptedException InterruptedException
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void measureThroughput() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+    /**
+     * 测试执行一次的平均时间
+     *
+     * @throws InterruptedException InterruptedException
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureAvgTime() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+    /**
+     * 测试同一时间不同样本
+     *
+     * @throws InterruptedException InterruptedException
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.SampleTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureSamples() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+    /**
+     * 冷启动，即第一次使用需要的时间
+     *
+     * @throws InterruptedException InterruptedException
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.SingleShotTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureSingleShot() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+    /**
+     * 需要测试的维度
+     *
+     * @throws InterruptedException InterruptedException
+     */
+    @Benchmark
+    @BenchmarkMode({Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime})
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureMultiple() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+    /**
+     * 测试所有维度
+     *
+     * @throws InterruptedException InterruptedException
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureAll() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMH03BenchmarkModes.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
