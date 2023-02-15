84a6701639e7a5fa5dcd869b22aea24adfebc651
xianzhan <424447832@qq.com>
2023-02-15 23:23:33 +0800

    add: spring SingleTest

---
+++ frame-spring/spring-base/src/test/java/xianzhan/frame/spring/base/single/SingleTest.java
@@ -0,0 +1,23 @@
+package xianzhan.frame.spring.base.single;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author xianzhan
+ * @since 2023-02-15
+ */
+public class SingleTest {
+
+    @Test
+    public void test() {
+        var packageName = SingleTest.class.getPackageName();
+        ApplicationContext context = new AnnotationConfigApplicationContext(packageName);
+
+        ISingleBean bean = context.getBean(ISingleBean.class);
+        System.out.println(bean);
+        Assertions.assertNotNull(bean);
+    }
+}
---
+++ frame-spring/spring-base/src/test/java/xianzhan/frame/spring/base/single/impl/SingleBeanImpl.java
@@ -0,0 +1,16 @@
+package xianzhan.frame.spring.base.single.impl;
+
+import org.springframework.stereotype.Service;
+import xianzhan.frame.spring.base.single.ISingleBean;
+
+/**
+ * 单例 bean 实现
+ *
+ * @author xianzhan
+ * @since 2023-02-15
+ */
+@Service
+public class SingleBeanImpl implements ISingleBean {
+
+
+}
