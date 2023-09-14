d75ef094408d86e492d66964e08109c7d3e893e7
xianzhan <424447832@qq.com>
2023-09-14 21:24:19 +0800

    add: awt

---
+++ frame-ui/ui-awt/src/main/java/xianzhan/frame/ui/awt/AWTMain.java
@@ -0,0 +1,23 @@
+package xianzhan.frame.ui.awt;
+
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+
+/**
+ * @author xianzhan
+ * @since 2023-08-13
+ */
+public class AWTMain {
+
+    public static void main(String[] args) {
+        var frame = new Frame("Title");
+        frame.setLocation(600, 300);
+        frame.setSize(500, 500);
+        frame.setBackground(Color.BLACK);
+
+        frame.add(new Button("Click me"));
+
+        frame.setVisible(true);
+    }
+}
