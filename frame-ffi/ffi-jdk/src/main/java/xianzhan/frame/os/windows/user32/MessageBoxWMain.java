ad17bab9721fbc3a12844c1f6a0a57f2429a6150
xianzhan <424447832@qq.com>
2023-12-19 23:11:27 +0800

    refactor: MessageBoxW

--- frame-ffi/ffi-jdk/src/main/java/xianzhan/frame/os/windows/user32/MessageBoxWMain.java
+++ frame-ffi/ffi-jdk/src/main/java/xianzhan/frame/os/windows/user32/MessageBoxWMain.java
@@ -1,12 +1,11 @@
-package xianzhan.frame.os.microsoft.user32;
+package xianzhan.frame.os.windows.user32;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
-import java.lang.foreign.Linker;
 import java.lang.foreign.MemorySegment;
-import java.lang.foreign.SymbolLookup;
 import java.lang.foreign.ValueLayout;
-import java.nio.charset.StandardCharsets;
+
+import static xianzhan.frame.os.windows.util.StrUtil.L;
 
 /**
  * MessageBoxW demo
@@ -17,13 +16,9 @@ import java.nio.charset.StandardCharsets;
 public class MessageBoxWMain {
 
     public static void main(String[] args) throws Throwable {
-        var linker = Linker.nativeLinker();
         try (var arena = Arena.ofConfined()) {
-            var user32 = SymbolLookup.libraryLookup("User32", arena);
-
             // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxw
-            var messageBoxW_MH = linker.downcallHandle(user32.find("MessageBoxW")
-                    .orElseThrow(() -> new RuntimeException("MessageBoxW Not Found")), FunctionDescriptor.of(
+            var messageBoxW_MH = User32.find("MessageBoxW", FunctionDescriptor.of(
                     // return int
                     ValueLayout.JAVA_INT,
                     // HWND hWnd
@@ -48,17 +43,6 @@ public class MessageBoxWMain {
             var result = (int) messageBoxW_MH.invokeExact(NULL, lpText, lpCaption, MB_OK);
             System.out.println(STR."MessageBox result: \{result}");
         }
-    }
-
-    private static MemorySegment L(Arena arena, String str) {
-        // https://stackoverflow.com/questions/66072117/why-does-windows-use-utf-16le
-        var bs = str.getBytes(StandardCharsets.UTF_16LE);
-        var ms = arena.allocate(bs.length + Short.BYTES);
-        MemorySegment.copy(bs, 0, ms, ValueLayout.JAVA_BYTE, 0, bs.length);
-        // fix: 乱码 UTF_16LE 双字节
-        ms.set(ValueLayout.JAVA_SHORT, bs.length, (short) '\0');
-
-        return ms;
     }
 }
 
