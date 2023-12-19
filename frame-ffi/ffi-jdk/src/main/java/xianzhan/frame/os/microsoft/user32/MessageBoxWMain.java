0313e858e8173a22f3162b874606048585cebefd
xianzhan <424447832@qq.com>
2023-12-19 20:03:15 +0800

    update: ffi MemorySegment.copy

--- frame-ffi/ffi-jdk/src/main/java/xianzhan/frame/os/microsoft/user32/MessageBoxWMain.java
+++ frame-ffi/ffi-jdk/src/main/java/xianzhan/frame/os/microsoft/user32/MessageBoxWMain.java
@@ -3,7 +3,6 @@ package xianzhan.frame.os.microsoft.user32;
 import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
 import java.lang.foreign.Linker;
-import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.SymbolLookup;
 import java.lang.foreign.ValueLayout;
@@ -23,43 +22,41 @@ public class MessageBoxWMain {
             var user32 = SymbolLookup.libraryLookup("User32", arena);
 
             // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxw
-            var messageBoxW_MH = linker.downcallHandle(user32.find("MessageBoxW").get(), FunctionDescriptor.of(
-                    ValueLayout.JAVA_INT, // return int
-                    ValueLayout.ADDRESS, // HWND hWnd
-                    ValueLayout.ADDRESS.withTargetLayout(MemoryLayout.sequenceLayout(ValueLayout.JAVA_BYTE)), // LPCWSTR lpText
-                    ValueLayout.ADDRESS.withTargetLayout(MemoryLayout.sequenceLayout(ValueLayout.JAVA_BYTE)), // LPCWSTR lpCaption
-                    ValueLayout.JAVA_INT // UINT uType
+            var messageBoxW_MH = linker.downcallHandle(user32.find("MessageBoxW")
+                    .orElseThrow(() -> new RuntimeException("MessageBoxW Not Found")), FunctionDescriptor.of(
+                    // return int
+                    ValueLayout.JAVA_INT,
+                    // HWND hWnd
+                    ValueLayout.ADDRESS,
+                    // LPCWSTR lpText
+                    ValueLayout.ADDRESS,
+                    // LPCWSTR lpCaption
+                    ValueLayout.ADDRESS,
+                    // UINT uType
+                    ValueLayout.JAVA_INT
             ));
 
-            var NULL = MemorySegment.NULL; // HWND hWnd = NULL
-
-            // https://stackoverflow.com/questions/66072117/why-does-windows-use-utf-16le
-            var cs = StandardCharsets.UTF_16LE;
+            // HWND hWnd = NULL
+            var NULL = MemorySegment.NULL;
             // LPCWSTR lpText = "Hello from Panama! 你好, the world"
-//            var lpText = arena.allocateArray(ValueLayout.JAVA_BYTE,
-//                    "Hello from Panama! 你好, the world".getBytes(cs));
-            var lpText = allocateArray(arena, "Hello from Panama! 你好, the world");
+            var lpText = L(arena, "Hello from Panama! 你好, the world");
             // LPCWSTR lpCaption = "Demo123 你好，世界"
-//            var lpCaption = arena.allocateArray(ValueLayout.JAVA_BYTE, "Demo123 你好，世界".getBytes(cs));
-             var lpCaption = allocateArray(arena, "Demo123 你好，世界");
-
-            var MB_OK = 0x0; // UINT uType = MB_OK
+            var lpCaption = L(arena, "Demo123 你好，世界");
+            // UINT uType = MB_OK
+            var MB_OK = 0x0;
 
             var result = (int) messageBoxW_MH.invokeExact(NULL, lpText, lpCaption, MB_OK);
             System.out.println(STR."MessageBox result: \{result}");
         }
     }
 
-    private static MemorySegment allocateArray(Arena arena, String str) {
+    private static MemorySegment L(Arena arena, String str) {
+        // https://stackoverflow.com/questions/66072117/why-does-windows-use-utf-16le
         var bs = str.getBytes(StandardCharsets.UTF_16LE);
-        var ms = arena.allocate(bs.length + 2);
-        var i = 0;
-        for (; i < bs.length; i++) {
-            ms.set(ValueLayout.JAVA_BYTE, i, bs[i]);
-        }
+        var ms = arena.allocate(bs.length + Short.BYTES);
+        MemorySegment.copy(bs, 0, ms, ValueLayout.JAVA_BYTE, 0, bs.length);
         // fix: 乱码 UTF_16LE 双字节
-        ms.set(ValueLayout.JAVA_BYTE, i++, (byte) '\0');
-        ms.set(ValueLayout.JAVA_BYTE, i, (byte) '\0');
+        ms.set(ValueLayout.JAVA_SHORT, bs.length, (short) '\0');
 
         return ms;
     }
